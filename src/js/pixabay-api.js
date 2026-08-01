cb74e0ab0939fd72a08dcd1f3d2abc43bbaaacdd
Alice <varshavska.alina@gmail.com>
2026-07-31 21:58:41 -0600

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '56671223-37e43b5ca49c43ffc47d09138';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,64 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const loader = document.querySelector('.loader-container');
+
+const lightbox = new SimpleLightbox('.gallery a');
+export function clearGallery() {
+    gallery.innerHTML = '';
+};
+
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+};
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
+};
+
+export function createGallery(images) {
+  const markup = images
+    .map(image => {
+      return `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${image.largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${image.webformatURL}"
+              alt="${image.tags}"
+            />
+          </a>
+
+          <div class="image-info">
+            <div class="info-item">
+              <p class="info-label">Likes</p>
+              <p class="info-value">${image.likes}</p>
+            </div>
+
+            <div class="info-item">
+              <p class="info-label">Views</p>
+              <p class="info-value">${image.views}</p>
+            </div>
+
+            <div class="info-item">
+              <p class="info-label">Comments</p>
+              <p class="info-value">${image.comments}</p>
+            </div>
+
+            <div class="info-item">
+              <p class="info-label">Downloads</p>
+              <p class="info-value">${image.downloads}</p>
+            </div>
+          </div>
+        </li>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+
